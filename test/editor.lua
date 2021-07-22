4fa8f56dad2ec8b85efcad54d4df5416841d486a
Izzu <cassell.izzy@gmail.com>
2021-07-22 14:44:33 +0100

    map editor push

--- test/editor.lua
+++ test/editor.lua
@@ -57,6 +57,15 @@ function love.load()
 	prelude = love.audio.newSource("assets/01-prelude.mp3", "static")
 	--love.audio.play(prelude)
 
+	-- load animations
+		-- file, size, speed, repeat boolean
+	idle = hybrid.newAnimation("assets/Animation-idle.png", 32, 32, 8, true)
+	runr = hybrid.newAnimation("assets/Animation-runr.png", 32, 32, 8, true)
+	runl = hybrid.newAnimation("assets/Animation-runl.png", 32, 32, 8, true)
+
+	-- make a new player object with animations
+		-- animation table, x, y, speed
+	player = hybrid.newPlayer({idle, runr, runl}, 1, 3, 5)
 	-- new camera
 	camera = hybrid.newCamera()
 
@@ -87,15 +96,6 @@ function love.load()
 	--map:loadTextures(map.layers[3].textures, 16, 16, 2)
 	--map:loadCollisionBoxes(collisionBoxes, 2)
 
-	-- load animations
-		-- file, size, speed, repeat boolean
-	idle = hybrid.newAnimation("assets/Animation-idle.png", 32, 32, 8, true)
-	runr = hybrid.newAnimation("assets/Animation-runr.png", 32, 32, 8, true)
-	runl = hybrid.newAnimation("assets/Animation-runl.png", 32, 32, 8, true)
-
-	-- make a new player object with animations
-		-- animation table, x, y, speed
-	player = hybrid.newPlayer({idle, runr, runl}, 0, 0, 1)
 
 	--player.speed = 0.1*scale
 
@@ -104,6 +104,13 @@ function love.load()
 	-- required for the camera to work with the player and the map
 	camera:set(player, map)
 
+
+	mx = love.mouse.getX()
+	my = love.mouse.getY()
+
+	mmx	= math.floor((mx - camera.x)/scale)
+	mmy	= math.floor((my - camera.y)/scale)
+	map:clean()
 	--noise(map)
 end
 
@@ -114,7 +121,48 @@ end
 local y = 0
 local x = 0
 
+-- on a single mouseclick take v1, v2
+-- on mouse release take v3, v4
+
+function love.mousepressed(x, y, button, istouch, presses)
+	if mx < textures.x and button == 1
+		and textures.layer == "collision" then
+		local t = {}
+		t[1] = math.floor((mx - camera.x)/scale)
+		t[2] = math.floor((my - camera.y)/scale)
+		table.insert(map.collision, t)
+	end
+	if mx < textures.x and button == 2
+		and textures.layer == "collision" then
+		for i, v in pairs(map.collision) do
+			if math.floor((mx - camera.x)/scale) >= v[1]
+				and math.floor((my - camera.y)/scale) <= v[2]
+				and math.floor((mx - camera.x)/scale) <= v[3]
+				and math.floor((my - camera.y)/scale) >= v[4]
+				then
+					print(i, v, #map.collision)
+					if #map.collision == 0 then
+						map.collision = {}
+					end
+					print(table.remove(map.collision, i))
+			end
+		end
+	end
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+	if mx < textures.x and button == 1
+		and textures.layer == "collision" then
+		map.collision[#map.collision][3] = math.floor((mx - camera.x)/scale)
+		map.collision[#map.collision][4] = math.floor((my - camera.y)/scale)
+	end
+end
+
+function collision()
+end
+
 function love.update(dt)
+	player:setAnimation(1)
 	--print(love.graphics.getWidth()%map.texturesize)
 
 	--map.texturesize = map.texturesize + love.graphics.getWidth()%map.texturesize
@@ -149,6 +197,9 @@ function love.update(dt)
 	--print(player.x, player.y)
 	camera.x = camera.x + v[1]
 	camera.y = camera.y + v[2]
+
+	--player.x = player.x - v[1]
+	--player.y = player.y - v[2]
 	--anim:play()
 	--player.anims[1]:play()
 	for _, v in pairs(player.anims) do
@@ -157,7 +208,8 @@ function love.update(dt)
 
 	mx = love.mouse.getX()
 	my = love.mouse.getY()
-
+	mmx	= math.floor((mx - camera.x)/scale)
+	mmy	= math.floor((my - camera.y)/scale)
 
 	--camera.x = camera.x + v[1]
 	--camera.y = camera.y + v[2]
@@ -169,12 +221,40 @@ function love.update(dt)
 	-- sets anim.quad to the selection and anim.layer to the layer selected
 	textures:selection(mx, my, scale, map)
 
-	if love.mouse.isDown(1) and mx < textures.x then
-		local s = map.texturesize
-		local x = (mx - camera.x - (mx-camera.x)%s)/s + 1
-		local y = (my - camera.y - (my-camera.y)%s)/s + 1
+	--textures.layer = "collision"
 
+	--[[
+	if mx < textures.x and textures.layer == "collision" then
+		k = collisionk
+		if love.mouse.isDown(1) then
+			map.collision[k] = {}
+			if map.collision[k][1] == nil then
+			end
+		end
+		if love.mouse.isDown == false then
+			map.collision[k][3] = mx
+			map.collision[k][4] = my
+			collisionk = collisionk + 1
+		else
+			return
+		end
+	end
+	]]--
+
+	local s = map.texturesize
+	local x = (mx - camera.x - (mx-camera.x)%s)/s + 1
+	local y = (my - camera.y - (my-camera.y)%s)/s + 1
+	if love.mouse.isDown(1) and mx < textures.x and type(textures.layer) == "number" then
 		map:set(x, y, textures.layer, textures.quad)
+	end
+
+	if love.mouse.isDown(2) and mx < textures.x and type(textures.layer) == "number"  then
+		map:set(x, y, textures.layer, nil)
+	end
+
+	if love.mouse.isDown(1) and mx < textures.x and textures.layer == "player" then
+		player.x = mmx
+		player.y = mmy
 	end
 
 	player.dx = math.floor(player.x/scale)*scale
@@ -204,13 +284,23 @@ function love.draw()
 	scale = map.texturesize/map.pixelsize
 
 	love.graphics.push()
+	----------
 	love.graphics.scale(scale, scale)
 	map:draw(cx, cy, 1)
 	map:draw(cx, cy, 2)
+	player:draw(player.x+camera.x/scale, player.y+camera.y/scale)
 	map:draw(cx, cy, 3)
+	map:drawCollision()
 
 	textures:draw(scale)
+	----------
 	love.graphics.pop()
+
+	if textures.layer == "collision" then
+		love.graphics.print("collision (draw from bottom-left to top-right! right click to delete)", 0, 0, 0, 2)
+	else
+		love.graphics.print(textures.layer, 0, 0, 0, 5)
+	end
 
 	--print(string.format("fps:		%.0f\n", 1/(os.clock() - y)))
 end
